176d9b844a1fb1d1eb3b961fe84097667ac5c693
olivierpoirier <117477661+olivierpoirier@users.noreply.github.com>
2024-12-18 20:10:00 -0500

    remettre await

--- src/services/game.service.ts
+++ src/services/game.service.ts
@@ -4,7 +4,7 @@ export class MongoGameService {
 
   public static async getAllGames() {
     //https://mongoosejs.com/docs/api/model.html#Model.find()
-    return MongoGames.find();
+    return await MongoGames.find();
   }
   
 }
